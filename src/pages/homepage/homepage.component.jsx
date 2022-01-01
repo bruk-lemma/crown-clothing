7c077215916c7b1302d2b7c52316ae10390a462f
bruk-lemma <bruklemma2017@gmail.com>
2022-01-01 20:01:27 +0300

    broken our home page into smaller components ,finished styling

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import react from "react";
+
+import './homepage.style.scss';
+import Directory from "../../components/directory/directory.components";
+
+const HomePage=()=>(
+    <Directory/>
+)
+
+export default HomePage;
